15ea2b768f2266babcd30dcc2a9376d7cd8691c4
Mantas Skirmantas <mantas.skirmantas@yahoo.com>
2021-06-15 17:13:52 +0300

    Dynamic data replacement added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+
 // FILES
 
 // Blocking, synchronous way
@@ -34,38 +35,65 @@ const url = require('url');
 
 // SERVER
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+  }
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObject = JSON.parse(data);
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((request, response) => {
-   const pathName = request.url;
+    const pathName = request.url;
 
-   //Overview page
-   if (pathName === '/' || pathName === '/overview'){
-    response.end('This is the OVERVIEW')
+    //Overview page
+    if (pathName === '/' || pathName === '/overview'){
+
+    response.writeHead(200, {'Content-type': 'text/html'});
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el));
+    console.log(cardsHtml); // returns array of data strings as html. Needs to be joined into a single string
+
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+    response.end(output);
+
 
     //Product page
-   }else if(pathName === '/product'){
+    }else if(pathName === '/product'){
     response.end('This is the PRODUCT');
 
+
     //API
-   }else if(pathName === '/api'){
+    }else if(pathName === '/api'){
     response.writeHead(200, {'Content-type': 'application/json'});
     response.end(data);
 
+
     // Not found
-   }else{
-       response.writeHead(404, {
-           'Content-type': 'text/html',
-           'my-own-header': 'hello-world'
-       });
-       response.end('<h1>Page not found!</h1>');
-   }
+    }else{
+        response.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        response.end('<h1>Page not found!</h1>');
+    }
     
 });
 
 server.listen(8000, '127.0.0.1', ()=> { // 8000 is a PORT, 127.0.0.1 = standard IP address for localhost
     console.log('Listening to request on port 8000');
 }) 
-
-// 
